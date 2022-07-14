264395b9d4353cdf62a963418163687ecaa7649e
Ilya Veryuhanov <veruhanov00@mail.ru>
2022-07-14 13:51:58 +0400

    add 10program

---
+++ example10_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = {1, 2, 3, 4, 243, 5, 6, 44, 77, 12, 566, 243, 777, 245, 666};
+
+int n = array.Length;
+int find = 243;
+int index = 0;
+
+while(index < n)
+{
+    if (array[index] == find)
+  {Console.WriteLine(index);
+   break;
+            //   остановка после первого найденного числа
+  }
+    index++;
+}
